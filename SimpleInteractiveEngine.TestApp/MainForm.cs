a2d1b298398ef33b423ac91b129d62e28f58d191
Juraj Labovsky <juraj.labovsky@stuba.sk>
2026-08-26 16:38:42 +0200

    Add project files.

---
+++ SimpleInteractiveEngine.TestApp/MainForm.cs
@@ -0,0 +1,18 @@
+using SimpleInteractiveEngine;
+
+namespace SimpleInteractiveEngine.TestApp
+{
+    public partial class MainForm : Form
+    {
+        ActiveCanvas engine;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SimpleInteractiveEngine/ActiveCanvas.cs
@@ -0,0 +1,7 @@
+﻿namespace SimpleInteractiveEngine
+{
+    public partial class ActiveCanvas
+    {
+
+    }
+}

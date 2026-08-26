220300b551a871fe338fb75583ba57bfd4396988
Juraj Labovsky <juraj.labovsky@stuba.sk>
2026-08-26 18:50:52 +0200

    Added point and polyline drawing support

--- SimpleInteractiveEngine.TestApp/MainForm.Designer.cs
+++ SimpleInteractiveEngine.TestApp/MainForm.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             pictureBox1 = new PictureBox();
+            toolStrip1 = new ToolStrip();
+            toolStripButtonAddPoint = new ToolStripButton();
+            toolStripButtonAddPolyLine = new ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            toolStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // pictureBox1
@@ -40,21 +45,57 @@
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
+            // toolStrip1
+            // 
+            toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripButtonAddPoint, toolStripButtonAddPolyLine });
+            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 1;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonAddPoint
+            // 
+            toolStripButtonAddPoint.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButtonAddPoint.Image = (Image)resources.GetObject("toolStripButtonAddPoint.Image");
+            toolStripButtonAddPoint.ImageTransparentColor = Color.Magenta;
+            toolStripButtonAddPoint.Name = "toolStripButtonAddPoint";
+            toolStripButtonAddPoint.Size = new Size(23, 22);
+            toolStripButtonAddPoint.Text = "Add Point";
+            toolStripButtonAddPoint.Click += toolStripButtonAddPoint_Click;
+            // 
+            // toolStripButtonAddPolyLine
+            // 
+            toolStripButtonAddPolyLine.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            toolStripButtonAddPolyLine.Image = (Image)resources.GetObject("toolStripButtonAddPolyLine.Image");
+            toolStripButtonAddPolyLine.ImageTransparentColor = Color.Magenta;
+            toolStripButtonAddPolyLine.Name = "toolStripButtonAddPolyLine";
+            toolStripButtonAddPolyLine.Size = new Size(23, 22);
+            toolStripButtonAddPolyLine.Text = "Add PolyLine";
+            toolStripButtonAddPolyLine.Click += toolStripButtonAddPolyLine_Click;
+            // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(toolStrip1);
             Controls.Add(pictureBox1);
             Name = "MainForm";
             Text = "Form1";
             Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            toolStrip1.ResumeLayout(false);
+            toolStrip1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox pictureBox1;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButtonAddPoint;
+        private ToolStripButton toolStripButtonAddPolyLine;
     }
 }
--- SimpleInteractiveEngine.TestApp/MainForm.cs
+++ SimpleInteractiveEngine.TestApp/MainForm.cs
@@ -17,6 +17,23 @@ namespace SimpleInteractiveEngine.TestApp
         private void Form1_Load(object sender, EventArgs e)
         {
             DemoScene.Build(engine);
+
+            
+            
+            this.KeyPreview = true;
+            this.KeyDown += (s, e) => { if (e.KeyCode == Keys.Escape) engine.CancelDrawing(); };
+            this.KeyDown += (s, e) => { if (e.KeyCode == Keys.Enter) engine.FinishDrawing();};
+        }
+
+        private void toolStripButtonAddPoint_Click(object sender, EventArgs e)
+        {
+            engine.StartPlacingPoints(label: "New Point", brush: new SolidBrush(Color.Crimson), shape: ActiveCanvas.PointShape.Diamond, size: 8f, continuous: false);
+        }
+
+        private void toolStripButtonAddPolyLine_Click(object sender, EventArgs e)
+        {
+            engine.StartDrawingPolyline("Nová trasa", new Pen(Color.ForestGreen, 3f));
+             
         }
     }
 }
